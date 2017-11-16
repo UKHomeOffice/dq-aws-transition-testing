5139a3062f00f2f47147b1a43e5e1196d836d2ef
nick-otter <nicholas.a.otter@gmail.com>
2017-11-16 18:10:31 +0000

    Added e2e tests

--- terraform-runner/tests/runner_test.py
+++ terraform-runner/tests/runner_test.py
@@ -19,26 +19,37 @@ class TestRunnerMethods(unittest.TestCase):
         os_mock.assert_called_once_with("./.tmp")
 
 
-class TestE2E(unittest.TestCase): # pragma: no cover
-    def test_the_full_e2e(self):
-        snippet = """
-provider "aws" {
-    access_key = "${var.AWS_ACCESS_KEY}"
-    secret_key = "${var.AWS_SECRET_KEY}"
-    region     = "us-west-2"
-}
+class TestE2E(unittest.TestCase):
 
-resource "aws_instance" "foo" {
-  ami           = "foo"
-  instance_type = "t2.micro"
-}
-"""
-        result = Runner(snippet).result
-        self.assertEqual(result["destroy"], False)
-        self.assertEqual(result["aws_instance.foo"]["instance_type"], "t2.micro")
-        self.assertEqual(result["aws_instance.foo"]["ami"], "foo")
-        self.assertEqual(result["aws_instance.foo"]["destroy"], False)
-        self.assertEqual(result["aws_instance.foo"]["destroy_tainted"], False)
+    def setUp(self):
+        self.snippet = """
+        provider "aws" {
+            access_key = "${var.AWS_ACCESS_KEY}"
+            secret_key = "${var.AWS_SECRET_KEY}"
+            region     = "us-west-2"
+        }
+
+        resource "aws_instance" "foo" {
+          ami           = "foo"
+          instance_type = "t2.micro"
+        }
+        """
+        self.result = Runner(self.snippet).result
+
+    def test_root_destroy(self):
+        self.assertEqual(self.result["destroy"], False)
+
+    def test_instance_type(self):
+        self.assertEqual(self.result["aws_instance.foo"]["instance_type"], "t2.micro")
+
+    def test_ami(self):
+        self.assertEqual(self.result["aws_instance.foo"]["ami"], "foo")
+
+    def test_destroy(self):
+        self.assertEqual(self.result["aws_instance.foo"]["destroy"], False)
+
+    def test_destroy_tainted(self):
+        self.assertEqual(self.result["aws_instance.foo"]["destroy_tainted"], False)
 
 if __name__ == '__main__':
     unittest.main()
